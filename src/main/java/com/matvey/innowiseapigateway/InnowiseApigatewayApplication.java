dab77db50921f1c33e3120117d0f7aec3e2bec85
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-06-18 19:23:09 +0300

    init

---
+++ src/main/java/com/matvey/innowiseapigateway/InnowiseApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.matvey.innowiseapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseApigatewayApplication.class, args);
+    }
+
+}
